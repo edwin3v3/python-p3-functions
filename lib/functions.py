56f356e1e124368cbd571b58cf5c1d867a6b7020
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-08-12 15:10:13 +0300

    Edited to greet_with_default function

--- lib/functions.py
+++ lib/functions.py
@@ -9,7 +9,10 @@ def greet(name):
 greet("Jacob")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("David")
 
 def add(num1, num2):
     pass
